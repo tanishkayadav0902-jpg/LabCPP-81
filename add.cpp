c6d1d64ba0d1b4e92d01d1c026d4cd5a1579f5a1
tanishkayadav0902-jpg <tanishkayadav0902@gmail.com>
2026-07-23 10:13:31 +0000

    assignmenrs

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+//using namespace std;
+int main()
+{
+int num1,num2,sum,avg;
+std::cout<<"Enter two numbers";
+std::cin>>num1;
+std::cin>>num2;
+sum=num1+num2;
+avg=sum/2;
+std::cout<<"sum="<<sum<<"\t Average="<<avg;
+return 0;
+}
+
+
