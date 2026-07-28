8e87f9110b3e3f6058a28410ac994ad8827de893
tanishkayadav0902-jpg <tanishkayadav0902@gmail.com>
2026-07-28 04:14:32 +0000

    student information

---
+++ student_info.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name      : Tanishka Yadav\n");
+    printf("Roll No.  : 81\n");
+    printf("Class     : SY Btech\n");
+    printf("Division  : C\n");
+
+    return 0;
+}
