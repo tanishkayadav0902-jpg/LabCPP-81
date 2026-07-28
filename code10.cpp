55a3131b4ad733e0a101cce1dc323e1eb135d832
tanishkayadav0902-jpg <tanishkayadav0902@gmail.com>
2026-07-28 03:40:51 +0000

    cpp10

---
+++ code10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    while (i <= n) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
