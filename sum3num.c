293b2f8d640634c3078fa8c10f765dcf73b92f13
tanishkayadav0902-jpg <tanishkayadav0902@gmail.com>
2026-07-28 04:20:53 +0000

    sum of 3 numbers

---
+++ sum3num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, sum;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    sum = num1 + num2 + num3;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
