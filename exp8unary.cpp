746a199e3bd45a9057831fff27e23d6c711f50e5
tanishkayadav0902-jpg <tanishkayadav0902@gmail.com>
2026-09-22 09:20:45 +0000

    unary operator overloading

---
+++ exp8unary.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Distance
+{
+public:
+    int feet, inch;
+
+    Distance(int f, int i)
+    {
+        this->feet = f;
+        this->inch = i;
+    }
+
+    // Overloading unary - operator
+    void operator-()
+    {
+        feet--;
+        inch--;
+
+        cout << "\nFeet & Inches (Decrement): "
+             << feet << "'" << inch;
+    }
+};
+
+int main()
+{
+    Distance d1(8, 9);
+
+    -d1;
+
+    return 0;
+}
